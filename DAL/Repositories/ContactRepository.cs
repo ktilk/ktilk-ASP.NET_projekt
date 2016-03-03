823a1c5d0660356f78ab3341f6a74d40cc477e12
Kaspar Tilk <ktilk@itcollege.ee>
2016-03-03 14:58:11 +0200

    Loodud mõned interfaced ja repod

---
+++ DAL/Repositories/ContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using Domain;
+
+namespace DAL.Repositories
+{
+    public class ContactRepository : EFRepository<Contact>, IContactRepository
+    {
+        public ContactRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
